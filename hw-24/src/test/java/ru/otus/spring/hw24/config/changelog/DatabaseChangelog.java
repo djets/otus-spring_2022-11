056bc3adea7e26e875c6f3733bf0667ece069033
djets <jag0s3@gmail.com>
2023-09-06 15:39:45 +0300

    hw24 add test

---
+++ hw-24/src/test/java/ru/otus/spring/hw24/config/changelog/DatabaseChangelog.java
@@ -0,0 +1,16 @@
+package ru.otus.spring.hw24.config.changelog;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.mongodb.client.MongoDatabase;
+import lombok.extern.slf4j.Slf4j;
+
+@ChangeLog
+@Slf4j
+public class DatabaseChangelog {
+
+    @ChangeSet(order = "001", id = "dropDb", author = "djet", runAlways = true)
+    public void dropDb(MongoDatabase db) {
+        db.drop();
+    }
+}
